e0b146d3eb4056134194e58d614bbfd5b0fc3132
2777018353 <2777018353@qq.com>
2020-03-20 11:27:12 +0800

    Calculate the percent of taking a taxi home.

--- Random_walk/random_walk_2d_in_number.py
+++ Random_walk/random_walk_2d_in_number.py
@@ -9,6 +9,15 @@ def random_walk_v2(n):
 
     return (x, y)
 
-for i in range(100):
-    one_walk = random_walk_v2(20)
-    print(one_walk, 'How far away from (0, 0): ', abs(one_walk[0]) + abs(one_walk[1]))
+taking_a_taxi_counter = 0
+how_many_simulations = 1000
+how_many_steps = 500
+leg_limit = 1000
+
+for i in range(how_many_simulations):
+    one_walk = random_walk_v2(how_many_steps)
+    how_far = int(one_walk[0]) ** 2 + int(one_walk[1]) ** 2
+    if how_far >= leg_limit:
+        taking_a_taxi_counter += 1
+
+print(taking_a_taxi_counter / how_many_simulations * 100, "% of taking a taxi home.")
