67c315feb1df508c578bc6b389b226c890cc95ba
2777018353 <2777018353@qq.com>
2020-03-20 20:53:23 +0800

    Draw 100 smart fool's game process.

--- Random_walk/give_the_number.py
+++ Random_walk/give_the_number.py
@@ -13,28 +13,66 @@ def give_the_number():
         return True
 
 
-def a_fool(funds, wager, game_count):
-    current_game_count = 0
+def a_smart_fool(funds, initial_wager, game_count):
+    wager = initial_wager
     x = []
     y = []
-    while current_game_count < game_count:
-        if give_the_number():
-            funds += wager
-            x.append(current_game_count)
-            y.append(funds)
-        else:
-            funds -= wager
-            x.append(current_game_count)
-            y.append(funds)
+    current_game_count = 1
+    previous_game = "win"
+    previous_game_wager = initial_wager
+
+    while current_game_count <= game_count:
+        if previous_game == "win":
+            print("The samrt fool win the last game~ Play more~")
+            if give_the_number():
+                funds += wager
+                print(funds)
+                x.append(current_game_count)
+                y.append(funds)
+            else:
+                funds -= wager
+                previous_game = "loss"
+                print(funds)
+                previous_game_wager = wager
+                x.append(current_game_count)
+                y.append(funds)
+
+                if funds < 0:
+                    print("The samrt fool went broke in {} games.".format(current_game_count))
+                    break
+
+        elif previous_game == "loss":
+            print("The smart fool lost the last one, but he is smart, and he will double up!")
+            if give_the_number():
+                wager = previous_game_wager * 2
+                print("The fool won", wager)
+                funds += wager
+                previous_game = "win"
+                wager = initial_wager
+                x.append(current_game_count)
+                y.append(funds)
+            else:
+                wager = previous_game_wager * 2
+                print("The fool lost", wager)
+                funds -= wager
+                print(funds)
+                previous_game = "loss"
+                previous_game_wager = wager
+                x.append(current_game_count)
+                y.append(funds)
+                if funds < 0:
+                    print("The smart fool went broke in", current_game_count, "games")
+                    break
         current_game_count += 1
-    if funds <= 0:
-        funds = "Penny less"
+    print(funds)
     plt.plot(x, y)
+
 
 n = 0
 while n < 100:
-    a_fool(1000000, 100000, 10000)
+    a_smart_fool(10000, 1000, 1000)
     n += 1
-plt.xlabel("How many games does the fool played")
+
+plt.xlabel("How many games the fool played")
 plt.ylabel("Funds")
 plt.show()
