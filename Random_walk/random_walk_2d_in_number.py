903f859b0476eaafc14f67cee55742e159b7b31e
2777018353 <2777018353@qq.com>
2020-03-20 10:46:32 +0800

    Upgrade to version 2.

--- Random_walk/random_walk_2d_in_number.py
+++ Random_walk/random_walk_2d_in_number.py
@@ -1,25 +1,14 @@
 import random
 
-def random_walk(n):
-    x = 0
-    y = 0
+def random_walk_v2(n):
+    x, y = (0, 0)
     for i in range(n):
-        step = random.choice(['N','S','W','E'])
-        if step == 'N':
-            y = y + 1
-        elif step == 'S':
-            y = y - 1
-        elif step == 'W':
-            x = x - 1
-        else:
-            x = x + 1
+        (dx, dy) = random.choice([(0,1),(0,-1),(-1, 0),(1, 0)])
+        x += dx
+        y += dy
+
     return (x, y)
 
-
-for i in range(50):
-    one_walk = random_walk(100)
-    how_far = int(one_walk[0]) ** 2 + int(one_walk[1]) ** 2
-    if how_far >= 50:
-        print(one_walk, 'It is too far away from home~ Taxi~~~')
-    else:
-        print(one_walk, "emmmmm, I haven't drunk, give me more drink!")
+for i in range(100):
+    one_walk = random_walk_v2(20)
+    print(one_walk, 'How far away from (0, 0): ', abs(one_walk[0]) + abs(one_walk[1]))
